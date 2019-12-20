a3ccdfd4ba4ea36d9590418af7ea2f906ae185d0
Badma <andraev@ya.ru>
2019-12-20 23:24:25 +0300

    first commit

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,19 @@
+const cookieClick = document.getElementById('cookie');
+cookieClick.onclick = function() {
+	const clicker = document.getElementById('clicker__counter');
+	clicker.textContent++;
+	if(clicker.click) {
+		cookieClick.width = 250;
+	} 
+}
+
+
+
+
+
+/*let cookieClick = document.getElementById('cookie');
+cookieClick.onclick = function() {
+	const clicker = document.getElementById('clicker__counter');
+	clicker.textContent ++;
+	cookieClick.width = 250;
+}*/
