5aa1b9b9efc6f3b25be1c913d6b94e8cc9c14e63
Badma <andraev@ya.ru>
2019-12-22 17:36:37 +0300

    verification task

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,9 +2,11 @@ const cookieClick = document.getElementById('cookie');
 cookieClick.onclick = function() {
 	const clicker = document.getElementById('clicker__counter');
 	clicker.textContent++;
-	if(clicker.click) {
+	if (clicker.textContent % 2 === 0) {
 		cookieClick.width = 250;
-	} 
+	} else {
+		cookieClick.width = 200;
+	}
 }
 
 
