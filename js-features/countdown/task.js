27bf3da995112b49df0271ad05d6555a763234e2
Badma <andraev@ya.ru>
2019-12-22 12:08:58 +0300

    first commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,9 @@
+let counter = setInterval(() => {
+	const timer = document.getElementById('timer');
+	if (timer.textContent == 0) {
+		alert('Вы победили в конкурсе');
+		clearInterval(counter);
+	} else {
+		timer.textContent -= 1;
+	}
+}, 1000)
