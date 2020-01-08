f544d6509733b49e64a30333308214455b52f0c5
Badma <andraev@ya.ru>
2020-01-08 10:35:09 +0300

    commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,10 +1,8 @@
 const arrow_prew = document.querySelector('.slider__arrow_prev');
 const arrow_next = document.querySelector('.slider__arrow_next');
 
-
 const images = document.querySelectorAll('.slider__item');
 let i = 0;
-
 
 arrow_prew.onclick = function () {
   images[i].className = 'slider__item';
@@ -16,7 +14,6 @@ arrow_prew.onclick = function () {
 
   images[i].className = 'slider__item slider__item_active';
 }
-
 
 arrow_next.onclick = function () {
   images[i].className = 'slider__item';
