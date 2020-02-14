e5fb76dc08fbe42b4286f25a9ec11f7d5f5d5a57
Badma <andraev@ya.ru>
2020-02-14 12:39:36 +0300

    book-reader

---
+++ dom/book-reader/task.js
@@ -0,0 +1,23 @@
+const fontSize = Array.from(document.querySelectorAll('.font-size'));
+const book = document.getElementById('book');
+
+function fontSizeActive(e) {
+	e.preventDefault();
+	const container = e.target;
+	for (let a in fontSize) {
+		fontSize[a].classList.remove('font-size_active');
+		book.classList.remove('font-size_big');
+		book.classList.remove('font-size_small');
+	}
+
+	container.classList.add('font-size_active');
+	if (container.classList.contains('font-size_small')) {
+		book.className = 'font-size_small';
+	} else if (container.classList.contains('font-size_big')) {
+		book.className = 'font-size_big'
+	}
+};
+
+for (let a in fontSize) {
+	fontSize[a].addEventListener('click', fontSizeActive);
+}
